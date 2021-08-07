6dc18c8904a9a985fff63d1be8a74ac832ca3a3c
Deyvison <deyvisonuchoa@hotmail.com>
2021-08-07 16:07:10 -0300

    watch page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import './app.sass';
 import Home from './pages/home/Home';
+import Watch from './pages/watch/Watch';
 
 function App() {
   return (
-    <Home/>
+    <Watch/>
   )};
 
 export default App;
