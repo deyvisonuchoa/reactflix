6336ba6628fe68a71fee94a1dfc7fbfd57288573
Deyvison <deyvisonuchoa@hotmail.com>
2021-08-09 18:04:49 -0300

    Login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './app.sass';
 import Home from './pages/home/Home';
+import Login from './pages/login/Login';
 import Register from './pages/register/Register';
 import Watch from './pages/watch/Watch';
 
 function App() {
   return (
-    <Register/>
+    <Login/>
   )};
 
 export default App;
